828a715368d5b4574009cc0bdd85c487c588fb88
k v s kiran <kvskiran@ks-MacBook-Pro.local>
2019-11-07 22:40:22 +0530

    removed key

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  googleCloudVisionAPIKey: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCs6-yRMGVrKliZ082dyClpSwwdE22hiLg",
-  googleSearchAPI: "https://www.googleapis.com/customsearch/v1?key=AIzaSyD6rT3yzndawuwb0y2WdrcbfkTrLhG_4c8&cx=009024791238972097032:zxk5rtsl18g&q="
-};
